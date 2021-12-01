972e66b4fab2ef4c13c05454ff9041b4b7241aaa
tptarah52 <tarahp@sas.upenn.edu>
2021-12-01 16:06:03 -0500

    personal data

--- projects/personal-data/assets/js/main.js
+++ projects/personal-data/assets/js/main.js
@@ -1,15 +1,26 @@
-document.getElementById("five").addEventListener("click", expandBlob);
+
+// function expandBlob() {
+//   var blob = document.getElementById("yellowblob");
+//         var currWidth = blob.clientWidth;
+//         var currHeight = blob.clientHeight;
+//         if(currWidth == 442){
+//         } else{
+//             blob.style.width = (currWidth + 50) + "px";
+//             blob.style.height = (currHeight + 50) + "px";
+//         } 
+// }
+
+let button = document.querySelector("#five");
+let blob = document.querySelector("#yellowblob");
 
 function expandBlob() {
-  var blob = document.getElementById("yellowblob");
-        var currWidth = blob.clientWidth;
-        var currHeight = blob.clientHeight;
-        if(currWidth == 442){
-        } else{
-            blob.style.width = (currWidth + 50) + "px";
-            blob.style.height = (currHeight + 50) + "px";
-        } 
+  blob.classList.toggle("large");
 }
+
+blob.addEventListener("click", expandBlob);
+
+//look at foreach function from week10b wednesday
+
 
 
     
