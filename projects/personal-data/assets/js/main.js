d03f824608f325e2ca2d78f4f12fa15f6c8335c5
tptarah52 <tarahp@sas.upenn.edu>
2021-12-01 13:42:23 -0500

    personal data

--- projects/personal-data/assets/js/main.js
+++ projects/personal-data/assets/js/main.js
@@ -1,5 +1,15 @@
-document.getElementById("one").addEventListener("click", expandBlob);
+document.getElementById("five").addEventListener("click", expandBlob);
 
 function expandBlob() {
-  document.getElementById("one").innerHTML = "YOU CLICKED ME!";
+  var blob = document.getElementById("yellowblob");
+        var currWidth = blob.clientWidth;
+        var currHeight = blob.clientHeight;
+        if(currWidth == 442){
+        } else{
+            blob.style.width = (currWidth + 50) + "px";
+            blob.style.height = (currHeight + 50) + "px";
+        } 
 }
+
+
+    
